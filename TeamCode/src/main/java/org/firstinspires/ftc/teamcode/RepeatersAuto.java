f5ec2600b649def41d19b272f70275bb37f61ad8
Michael Lin <Michael Lin>
2017-03-02 21:08:38 +0800

    repeatersauto update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RepeatersAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RepeatersAuto.java
@@ -1,9 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import for_camera_opmodes.LinearOpModeCamera;
+
 /**
  * Created by micro on 26-Feb-17.
  */
+@Autonomous(name="Repeaters Autonomous", group="Repeaters")
+public class BananaAuto extends LinearOpModeCamera {
+    private RepeatersHardware robot = new RepeatersHardware();
+    private ElapsedTime runtime = new ElapsedTime();
 
-public class RepeatersAuto {
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update
+
+        robot.init(hardwareMap);
+
+        runtime.reset();
+
+
+    }
 
 }
