720eac6cb68ef68e0539e346321136b7efec54d1
Michael02lin <michael02lin@gmail.com>
2017-03-06 16:59:33 +0800

    reapeaters op update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RepeatersOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RepeatersOp.java
@@ -49,19 +49,22 @@ public class RepeatersOp extends OpMode
         telemetry.addData("Status", "Running: " + runtime.toString());
 
         // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
-        robot.leftMotor.setPower(-gamepad1.left_stick_y);
-        robot.rightMotor.setPower(-gamepad1.right_stick_y);
+        if (gamepad1.left_trigger > 0 || gamepad1.right_trigger > 0) {
+            // function to turn robot 180 degree
+            robot.leftMotor.setPower(-gamepad1.left_trigger + gamepad1.right_trigger);
+            robot.rightMotor.setPower(gamepad1.left_trigger - gamepad1.right_trigger);
+        } else {
+            robot.leftMotor.setPower(-gamepad1.left_stick_y);
+            robot.rightMotor.setPower(-gamepad1.right_stick_y);
+        }
 
-        // function to turn robot 180 degree
-        robot.leftMotor.setPower(-gamepad1.left_trigger + gamepad1.right_trigger);
-        robot.rightMotor.setPower(gamepad1.left_trigger - gamepad1.right_trigger);
 
         robot.collectorMotor.setPower(gamepad2.left_trigger - gamepad2.right_trigger);
-        robot.elevatorMotor.setPower(gamepad2.left_trigger - gamepad2.right_trigger);
+        robot.elevatorMotor.setPower(- gamepad2.left_stick_y + gamepad2.left_stick_y);
 
-        if (gamepad2.x) {
+        if (gamepad1.x) {
             robot.flickerMotor.setPower(1);
-        } else if (gamepad2.y) {
+        } else if (gamepad1.y) {
             robot.flickerMotor.setPower(-1);
         } else {
             robot.flickerMotor.setPower(0);
