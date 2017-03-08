fe95e5cd8017af49a0f240e3ad2ebe485954ca6c
Michael02lin <michael02lin@gmail.com>
2017-03-08 15:33:18 +0800

    current changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RepeatersAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RepeatersAuto.java
@@ -39,7 +39,7 @@ public class RepeatersAuto extends LinearOpModeCamera {
             robot.rightMotor.setPower(-1);
             robot.leftMotor.setPower(-1);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 1)) {
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
             telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
         }
@@ -59,7 +59,7 @@ public class RepeatersAuto extends LinearOpModeCamera {
             robot.rightMotor.setPower(-1);
             robot.leftMotor.setPower(-1);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 2)) {
+        while (opModeIsActive() && (runtime.seconds() < 1)) {
             telemetry.addData("Path", "Leg 3: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
         }
