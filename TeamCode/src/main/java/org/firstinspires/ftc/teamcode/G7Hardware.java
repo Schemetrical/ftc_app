7d2f339eed29e50353ab9e018350adf898cfc39c
Schemetrical <Schemetrical@icloud.com>
2017-03-14 14:46:24 +0800

    unfucked some stuff, added g7hardware

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/G7Hardware.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Yichen Cao on 2017-02-25.
+ */
+
+class G7Hardware {
+    /* Public OpMode members. */
+    /* Declare OpMode members. */
+    DcMotor motorFront, motorLeft, motorRight, motorBack;
+
+    DcMotorSimple[] allMotors;
+
+    static final double[] MOTOR_XY = {1, 0, 0, 1, 0, -1, -1, 0};
+
+    /* Constructor */
+    G7Hardware() {
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+
+        // Define and Initialize Motors
+        motorFront = ahwMap.dcMotor.get("mf");
+        motorLeft = ahwMap.dcMotor.get("ml");
+        motorRight = ahwMap.dcMotor.get("mr");
+        motorBack = ahwMap.dcMotor.get("mb");
+
+        allMotors = new DcMotorSimple[]{
+                motorFront,
+                motorLeft,
+                motorRight,
+                motorBack
+        };
+
+        // Set all motors to zero power
+        for (DcMotorSimple motor: allMotors) {
+            motor.setPower(0);
+        }
+    }
+
+    void move(double left, double right) {
+        motorLeft.setPower(left);
+        motorRight.setPower(right);
+    }
+
+    // clockwise rotation = positive power
+    void rotate(double power) {
+        motorLeft.setPower(power);
+        motorRight.setPower(-power);
+    }
+}
+
