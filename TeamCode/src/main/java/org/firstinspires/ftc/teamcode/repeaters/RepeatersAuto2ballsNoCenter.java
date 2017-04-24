a42652280be7df0aa7085a12682b79c9382effbc
Michael02lin <michael02lin@gmail.com>
2017-04-24 13:26:07 +0800

    repeatersauto new options

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/repeaters/RepeatersAuto2ballsNoCenter.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.repeaters;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import for_camera_opmodes.LinearOpModeCamera;
+
+/**
+ * Created by MichaelL on 4/24/17.
+ */
+
+@Autonomous(name="Repeaters Autonomous", group="Repeaters")
+public class RepeatersAuto2ballsNoCenter extends LinearOpModeCamera {
+    /* Declare OpMode members. */
+    private RepeatersHardware robot = new RepeatersHardware();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for game start (driver press PLAY)
+        waitForStart();
+        // Step 0:  time delay
+        sleep(15000);
+        // Step 1:  Drive forward to aim
+        robot.rightMotor.setPower(-1);
+        robot.leftMotor.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.7)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        // Step 1:  Shoot
+        robot.rightMotor.setPower(0);
+        robot.leftMotor.setPower(0);
+        robot.flickerMotor.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2)) {
+            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Step 2:  Start elevator to load second ball
+        robot.flickerMotor.setPower(0);
+        robot.elevatorMotor.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 3)) {
+            telemetry.addData("Path", "Leg 3: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Step 3:  Shoot second ball
+        robot.elevatorMotor.setPower(0);
+        robot.flickerMotor.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2)) {
+            telemetry.addData("Path", "Leg 4: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/repeaters/RepeatersAutoNoCenter.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.repeaters;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import for_camera_opmodes.LinearOpModeCamera;
+
+/**
+ * Created by MichaelL on 4/24/17.
+ */
+
+@Autonomous(name="Repeaters Autonomous", group="Repeaters")
+public class RepeatersAutoNoCenter extends LinearOpModeCamera {
+    /* Declare OpMode members. */
+    private RepeatersHardware robot = new RepeatersHardware();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for game start (driver press PLAY)
+        waitForStart();
+
+        // Step 0:  time delay
+        sleep(15000);
+        // Step 1:  Drive forward to aim
+        robot.rightMotor.setPower(-1);
+        robot.leftMotor.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.7)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        // Step 1:  Shoot
+        robot.rightMotor.setPower(0);
+        robot.leftMotor.setPower(0);
+        robot.flickerMotor.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2)) {
+            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+    }
+}
