8c82c709aac4ce8b3c1e35d9ad0e487a76b7908b
Michael02lin <michael02lin@gmail.com>
2017-05-02 19:54:26 +0800

    repeaters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/repeaters/RepeatersAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/repeaters/RepeatersAuto.java
@@ -15,6 +15,7 @@ public class RepeatersAuto extends LinearOpModeCamera {
     /* Declare OpMode members. */
     private RepeatersHardware robot = new RepeatersHardware();   // Use a Pushbot's hardware
     private ElapsedTime runtime = new ElapsedTime();
+    private static final double     DRIVE_SPEED             = 0.4;
 
     @Override
     public void runOpMode() {
@@ -35,18 +36,13 @@ public class RepeatersAuto extends LinearOpModeCamera {
         // Step 0:  time delay
             sleep(15000);
         // Step 1:  Drive forward to aim
-            robot.rightMotor.setPower(-1);
-            robot.leftMotor.setPower(-1);
-            robot.frightMotor.setPower(-1);
-            robot.fleftMotor.setPower(-1);
+            robot.move(DRIVE_SPEED,DRIVE_SPEED);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 0.4)) {
+        while (opModeIsActive() && (runtime.seconds() < 1)) {
             telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
         }
         // Step 1:  Shoot
-            robot.rightMotor.setPower(0);
-            robot.leftMotor.setPower(0);
             robot.flickerMotor.setPower(-1);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 2)) {
@@ -56,10 +52,7 @@ public class RepeatersAuto extends LinearOpModeCamera {
 
         // Step 2:  Drive forward to park on center vortex
             robot.flickerMotor.setPower(0);
-            robot.rightMotor.setPower(-1);
-            robot.leftMotor.setPower(-1);
-            robot.frightMotor.setPower(-1);
-            robot.fleftMotor.setPower(-1);
+            robot.move(DRIVE_SPEED,DRIVE_SPEED);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 1.5)) {
             telemetry.addData("Path", "Leg 3: %2.5f S Elapsed", runtime.seconds());
